eb33a8ae5196fb556f2792bc1ce098fcac4f2cfe
Derek Jones <djamesjones4@gmail.com>
2024-08-18 17:55:48 -0700

    updated enum logic in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+
+  enum user_type: %w[base admin super_admin admin_read_only]
+
+  def user_type=(val)
+    user_type = case val
+                when 0, "base",
+                  "base"
+                when 1, "admin"
+                  "admin"
+                when 2, "super_admin"
+                  "super_admin"
+                when 3, "admin_read_only"
+                  "admin_read_only"
+                end
+  end
 end
