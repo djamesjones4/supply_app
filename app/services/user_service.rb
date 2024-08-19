7711c7b63755ffc0bbe8ab1e02d3ee372ea5647e
Derek Jones <djamesjones4@gmail.com>
2024-08-18 17:54:28 -0700

    added exception and return logic

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -3,7 +3,17 @@ class UserService
     UserService.new()
   end
 
-  def create_new_user(first_name:, last_name:, email:)
-    User.create!(first_name: first_name, last_name: last_name, email: email)
+  def create_new_user(first_name:, last_name:, email:, user_type: "base")
+    user = User.create!(first_name: first_name, last_name: last_name, email: email, user_type: user_type)
+
+    if user.save
+      {
+        success: true,
+        exception_id: user.id
+      }
+    else
+      Rails.logger.error("Create new user error: ")
+      raise CreateUserException, "Unable to create a new user."
+    end
   end
 end
