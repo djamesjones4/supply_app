c6cf263878ca8780d2b217b86f31539f5bf1b607
Derek Jones <djamesjones4@gmail.com>
2024-08-18 17:53:45 -0700

    added resolver logic

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -7,6 +7,15 @@ module Mutations
     argument :first_name, String, required: true
     argument :last_name, String, required: true
     argument :email, String, required: true
-    # argument :user_type, Graphql
+    argument :user_type, Types::UserAuthType, required: true
+
+    def resolve(**args)
+      begin
+        UserService.build.create_new_user(first_name: args[:first_name], last_name: args[:last_name], email: args[:email], user_type: args[:user_type])
+      rescue StandardError => e
+        raise BadRequestError, "Could not create user"
+      end
+      super
+    end
   end
 end
