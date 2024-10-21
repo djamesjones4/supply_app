ad25f39c75d48d8fc483a1d5ed8e2fc6f81e0fcb
Derek Jones <djamesjones4@gmail.com>
2024-10-21 12:28:43 -0700

    ran cart_item migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_21_191719) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_21_192131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
   create_enum "user_auth_type", ["base", "admin", "super_admin", "admin_read_only"]
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -106,6 +116,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_21_191719) do
     t.integer "points"
   end
 
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
   add_foreign_key "carts", "users"
   add_foreign_key "order_details", "payment_details", column: "payment_details_id"
   add_foreign_key "order_details", "users"
