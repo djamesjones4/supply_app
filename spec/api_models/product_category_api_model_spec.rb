ac2f981adb68967029a4b52256cdd906a463ed4f
Derek Jones <djamesjones4@gmail.com>
2024-10-22 17:24:06 -0700

    added api model spec

---
+++ spec/api_models/product_category_api_model_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ProductCategoryApiModel do
+  it "sets instance variables and attributes" do
+    record = ProductCategory.new(name: 'product', description: 'category')
+    result = described_class.new(record)
+
+    expect(result).to be_an_instance_of(ProductCategoryApiModel)
+    expect(result.instance_variable_names).to eq ["@id", "@name", "@description", "@deleted_at", "@created_at", "@updated_at"]
+    expect(result.name).to eq 'product'
+    expect(result.description).to eq 'category'
+  end
+end
